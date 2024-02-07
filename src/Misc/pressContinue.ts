ec7d7acc68a76ea255a586269a28f1601cfbbe32
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-02-07 19:41:46 +0000

    [ADD] Function PressContinue - allow to only display and wait for key press

---
+++ src/Misc/pressContinue.ts
@@ -0,0 +1,20 @@
+import { prompt } from "enquirer";
+
+/**
+ * @async
+ * @returns {Promise<void>}
+ */
+
+async function pressContinue(): Promise<void> {
+    console.log("Press any key to continue");
+    const questions = [
+      {
+        type: "input",
+        name: "continue",
+        message: "Press any key to continue",
+      },
+    ];
+    await prompt(questions);
+}
+
+export default pressContinue;
