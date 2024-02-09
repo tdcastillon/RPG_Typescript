24df26c4c6eb6ed278503d49c0c7be2918ff0f1c
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-02-09 09:06:56 +0000

    [FIX] Attack Skill

--- src/Skills/Skills/Damage/Attack.Skill.ts
+++ src/Skills/Skills/Damage/Attack.Skill.ts
@@ -7,7 +7,7 @@ import GameEntity from "../../../Game/GameEntity";
 // enums
 import skillType from "../../Enum/SkillType.enum";
 import skillTarget from "../../Enum/SkillTarget.enum";
-import pressContinue from "../../../Misc/pressContinue";
+import waitTime from "../../../Misc/waitTime";
 
 class Attack extends ManaSkills {
     constructor() {
@@ -16,10 +16,12 @@ class Attack extends ManaSkills {
 
     public async useSkill(user: GameEntity, target: GameEntity) : Promise<void> {
         const damage = user.getStats().getProperty("ATK") - target.getStats().getProperty("DEF");
-        const variance = Math.random() * 0.4 + 0.8;
-        const final_damage = Math.floor(damage * variance);
+        const high = 1.2 * damage;
+        const low = 0.8 * damage;
+        const final_damage = Math.floor(Math.random() * (high - low) + low);
         target.getStats().setProperty("HP", target.getStats().getProperty("HP") - final_damage);
         console.log(user.getName() + " attacked " + target.getName() + " and dealt " + (final_damage > 0 ? (final_damage + " damage.")  :  "no damage."));
+        await waitTime(0.5)
         await super.useSkill(user, target);
     }
 }
