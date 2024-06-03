3d46d9f34889a6cccd588e7b0df43785133d7747
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-06-04 00:55:39 +0100

    [ADD] Comment for DB_Enemy_Entry and BeginHero

--- src/Enemy/Interface/DB_Enemy_Entry.ts
+++ src/Enemy/Interface/DB_Enemy_Entry.ts
@@ -1,7 +1,10 @@
 import Enemy from "../Class/Enemy";
 
 /**
- * Interface representing a database entry for an enemy
+ * @description Interface representing a database entry for an enemy
+ * @property {Enemy} enemy - the constructor for creating the enemy
+ * @property {number} min_lv - Minimum level at which the monster can be faced
+ * @property {number} max_lv - Maximum level at which the monster can be faced
  */
 interface DB_Enemy_Entry {
     enemy: Enemy;
--- src/Interfaces/BeginHero.ts
+++ src/Interfaces/BeginHero.ts
@@ -1,5 +1,10 @@
 import Job from "../Job/Class/Job";
 
+/**
+ * @description Interface to create a Hero in the Game
+ * @property {string} name - the name of the hero
+ * @property {Job} job - the job of the hero
+*/
 interface BeginHero {
     name: string;
     job: Job;
