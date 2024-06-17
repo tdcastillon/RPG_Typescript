c62a7f9456642f6f16f731f42f92de1d0bf25a83
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-06-17 11:28:41 +0200

    [EDIT] Rename for better use

--- src/Enemy/Function/getAvalaibleEntries.ts
+++ src/Enemy/Function/getAvalaibleEntries.ts
@@ -7,10 +7,10 @@ import DB_Enemy_Entry from "../Interface/DB_Enemy_Entry";
 */
 
 function getAvalaibleEnties(lv_medium: number) : Array<DB_Enemy_Entry> {
-    const db = require('../db');
+    const enemy_db = require('../db');
     let avalaible_monsters: Array<DB_Enemy_Entry> = [];
 
-    db.forEach((enemy_entry: DB_Enemy_Entry) => {
+    enemy_db.forEach((enemy_entry: DB_Enemy_Entry) => {
         if (enemy_entry.min_lv <= lv_medium && enemy_entry.max_lv >= lv_medium) {
             avalaible_monsters.push(enemy_entry);
         }
