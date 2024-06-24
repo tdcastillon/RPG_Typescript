d6c3c3751a6d4d137b7de7aa13840a318c9dca13
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-06-24 03:43:14 +0200

    [EDIT] possibility to add / remove multiple items

--- src/Inventory/Function/addItem.ts
+++ src/Inventory/Function/addItem.ts
@@ -1,12 +1,12 @@
 import Item from "../../Items/Class/Item";
 import itemInventory from "../Interface/itemInventory";
 
-function addItem(inventory: Array<itemInventory>, item: Item) {
+function addItem(inventory: Array<itemInventory>, item: Item, quantity: number = 1) {
     let itemIndex = inventory.findIndex((itemInventory) => itemInventory.item.getName() === item.getName());
     if (itemIndex !== -1) {
-        inventory[itemIndex].quantity++;
+        inventory[itemIndex].quantity += quantity;
     } else {
-        inventory.push({ item, quantity: 1 });
+        inventory.push({ item, quantity: quantity });
     }
 }
 
--- src/Inventory/Function/removeItem.ts
+++ src/Inventory/Function/removeItem.ts
@@ -1,10 +1,10 @@
 import itemInventory from "../Interface/itemInventory";
 
-function removeItem(inventory: Array<itemInventory>, item_name: string) {
+function removeItem(inventory: Array<itemInventory>, item_name: string, quantity: number = 1) {
     let itemIndex = inventory.findIndex((itemInventory) => itemInventory.item.getName() === item_name);
     if (itemIndex !== -1) {
-        inventory[itemIndex].quantity--;
-        if (inventory[itemIndex].quantity === 0) {
+        inventory[itemIndex].quantity -= quantity;
+        if (inventory[itemIndex].quantity <= 0) {
             inventory.splice(itemIndex, 1);
         }
     }
