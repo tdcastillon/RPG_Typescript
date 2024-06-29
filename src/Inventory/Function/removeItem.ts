77b569ed0f6f590a5ce3483fcde73152faec9eaa
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-06-30 00:58:31 +0200

    [ADD] Comment for removeItem function

--- src/Inventory/Function/removeItem.ts
+++ src/Inventory/Function/removeItem.ts
@@ -1,5 +1,12 @@
 import itemInventory from "../Interface/itemInventory";
 
+/**
+ * function to remove an item from the inventory
+ * 
+ * @param {Array<itemInventory>} inventory - inventory of the user
+ * @param {string} item_name - name of the item to remove
+ * @param {number} quantity - quantity of the item to remove - default 1
+ */
 function removeItem(inventory: Array<itemInventory>, item_name: string, quantity: number = 1) {
     let itemIndex = inventory.findIndex((itemInventory) => itemInventory.item.getName() === item_name);
     if (itemIndex !== -1) {
